5ef26d7c2ade31182e5685a63d6df6ef0b29c926
jwmoses8995 <jacob8995@gmail.com>
2023-06-20 14:08:33 -0700

    Final changes for utility components and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export const content = [
   "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 ];
 export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
+    },
+  },
   extend: {
     backgroundImage: {
       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
